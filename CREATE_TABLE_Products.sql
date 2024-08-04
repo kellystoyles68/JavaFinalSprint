4b1f5e87284c08b331481826316e3482fa924174
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-08-04 10:25:19 -0400

    commit database files

---
+++ CREATE_TABLE_Products.sql
@@ -0,0 +1,7 @@
+create table Products (
+	productID VARCHAR(50),
+	prodName VARCHAR(61),
+	price VARCHAR(50),
+	quantity INT,
+	sellerId INT
+);
---
+++ CREATE_TABLE_Users.sql
@@ -0,0 +1,6 @@
+create table Users (
+	Username VARCHAR(50),
+	password VARCHAR(50),
+	email VARCHAR(50),
+	role VARCHAR(6)
+);
