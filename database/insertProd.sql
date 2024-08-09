77894e7f75db7f26282d65adbca0019d80769b12
joy111joy <mitchel_joy@hotmail.com>
2024-08-09 01:24:30 -0230

    Added insert Products

---
+++ database/insertProd.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.products(
+	id, name, price, quantity, seller_id)
+	VALUES (1, 'Product1', 10, 100, 1),
+		(2, 'Product2', 20, 100, 2),
+		(3, 'Product3', 15, 100, 3),
+		(4, 'Product4', 40, 100, 4);
